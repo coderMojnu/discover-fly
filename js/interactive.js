d15943343508ae774528acbbde0861afc17c2842
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-01-30 21:15:23 +0600

    Done Assignment 4

--- js/interactive.js
+++ js/interactive.js
@@ -1,5 +1,4 @@
 function handleTicketCountAndPrice(ticketType, isIncrease) {
-    //console.log(getInputValue('firstClass'));
     const ticketNumber = getInputValue(ticketType);
     let newTicketNumber = ticketNumber;
     if (isIncrease == true) {
@@ -9,12 +8,11 @@ function handleTicketCountAndPrice(ticketType, isIncrease) {
         newTicketNumber = ticketNumber - 1;
     }
     document.getElementById(ticketType).value = newTicketNumber;
-    calculateSubTotal();
+    calculateTotal();
 }
-function bookNowBtn(){
-    confirm("Confirm Please");
-}
-function calculateSubTotal() {
+
+//function to calculate Subtotal, total and tax
+function calculateTotal() {
     const firstClassTicket = getInputValue('firstClass');
     const economyTicket = getInputValue('economy');
     const subTotal = firstClassTicket * 150 + economyTicket * 100;
@@ -24,8 +22,20 @@ function calculateSubTotal() {
     document.getElementById('tax').innerText = totalTax;
     document.getElementById('total').innerText = total;
 }
+
+//function to get Input Value from User
 function getInputValue(ticketType) {
     const NumberOfTicket = document.getElementById(ticketType);
     const ticketValue = parseInt(NumberOfTicket.value);
     return ticketValue;
+}
+
+//function to confirm purchase
+function bookNowBtn() {
+    const firstClassTicket = getInputValue('firstClass');
+    const economyTicket = getInputValue('economy');
+    const subTotal = firstClassTicket * 150 + economyTicket * 100;
+    const totalTax = subTotal * 0.1;
+    const total = subTotal + totalTax;
+    confirm(document.innerText = 'Press OK to confirm purchase' + '\n' + '_____________________________________________________________________________' + '\n' + 'First Class Ticket - ' + firstClassTicket + '\n' + 'Economy Ticket - ' + economyTicket + '\n' + 'Subtotal - ' + subTotal + '\n' + 'Tax - ' + totalTax + '\n' + 'Total - ' + total);
 }
